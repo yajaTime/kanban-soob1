971e674d3a6cc6beee8ae9704cc225d37dcfa616
Jo Soo Yoon <sooyoon126@gmail.com>
2022-06-22 20:10:26 +0900

    init

---
+++ src/test/java/yaja/kanban/KanbanApplicationTests.java
@@ -0,0 +1,13 @@
+package yaja.kanban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
